6cc1ab561ce345bf1bffce0ee615028b1dba142f
renukumar07 <renukumar90@gmail.com>
2022-03-27 00:02:29 +0530

    Added Hello.js

---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>
+                Hello Renu
+            </h1>
+        </div>
+    )
+}
+
+export default Hello;
