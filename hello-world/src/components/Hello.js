9ae85a9e1eb0ba42fc7e4f176c072534ea34e050
renukumar07 <renukumar90@gmail.com>
2022-03-27 00:07:41 +0530

    Updated hello.js with React.createElement equivalant code with h1 as inner text rather than child tag

--- hello-world/src/components/Hello.js
+++ hello-world/src/components/Hello.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>
-                Hello Renu
-            </h1>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <h1>
+    //             Hello Renu
+    //         </h1>
+    //     </div>
+    // )
+    return React.createElement('div', null, '<h1>Hello Renu</h1>')
 }
 
 export default Hello;
