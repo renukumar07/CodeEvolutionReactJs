8350306cec1c7963bc16f430299ee915e3f1d5ed
renukumar07 <renukumar90@gmail.com>
2022-04-11 18:56:51 +0530

    08 JSX: React.createElement accepting more than 3 parameters

--- hello-world/src/components/Hello.js
+++ hello-world/src/components/Hello.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 
 const Hello = () => {
-    // return (
-    //     <div>
-    //         <h1>
-    //             Hello Renu
-    //         </h1>
-    //     </div>
-    // )
-    return React.createElement('div', null, '<h1>Hello Renu</h1>')
+  // return (
+  //     <div>
+  //         <h1>
+  //             Hello Renu
+  //         </h1>
+  //     </div>
+  // )
+  return React.createElement('div', null, 'h1', 'Hello Renu')
 }
 
-export default Hello;
+export default Hello
