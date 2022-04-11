1dd6167a354a807f75e7101efec4c5504938432d
renukumar07 <renukumar90@gmail.com>
2022-04-11 20:46:01 +0530

    10 State: Added new component Message and use state to have default Welcome message and used a button to update the text using setState

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,22 +1,10 @@
 import './App.css'
-import Greet from './components/Greet'
-import Hello from './components/Hello'
-import Welcome from './components/Welcome'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </Greet>
-      <Greet name="Clark" heroName="Superman">
-        <button>Action</button>
-      </Greet>
-      <Greet name="Diana" heroName="Wonder Woman" />
-      <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Clark" heroName="Superman" />
-      <Welcome name="Diana" heroName="Wonder Woman" />
-      {/* <Hello/> */}
+      <Message />
     </div>
   )
 }
