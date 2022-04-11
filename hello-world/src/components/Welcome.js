dca135b2d0ac17f226cebfabccdc1354334ad45b
renukumar07 <renukumar90@gmail.com>
2022-04-11 20:02:16 +0530

    09 Props: props is immutable

--- hello-world/src/components/Welcome.js
+++ hello-world/src/components/Welcome.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class Welcome extends Component {
   render() {
+    this.props.name = 'Renu'
     return (
       <h1>
         Welcome {this.props.name} a.k.a {this.props.heroName}
