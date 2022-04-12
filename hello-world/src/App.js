47313609800563652aa7ba513486c375cd3401da
renukumar07 <renukumar90@gmail.com>
2022-04-13 01:58:15 +0530

    12 Destructuring props and state: Destructuring props in class body

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Greet from './components/Greet'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
       <Greet name="Diana" heroName="Wonder Woman" />
+      <Welcome name="Bruce" heroName="Batman" />
     </div>
   )
 }
