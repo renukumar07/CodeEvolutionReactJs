48ad555fd7419d7979ffa90f8e10df239477aa26
renukumar07 <renukumar90@gmail.com>
2022-04-13 02:46:39 +0530

    15 Methods as props: Use arrow funciton to pass a string/or any value back to parent

--- hello-world/src/components/ChildComponent.js
+++ hello-world/src/components/ChildComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function ChildComponent(props) {
   return (
     <div>
-      <button onClick={props.greetHandler}>Greet Button</button>
+      <button onClick={() => props.greetHandler('child')}>Greet Button</button>
     </div>
   )
 }
