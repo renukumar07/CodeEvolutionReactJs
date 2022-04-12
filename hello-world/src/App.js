dde2ea11c3d58f74d38c7abe9d0b35b88ff333ff
renukumar07 <renukumar90@gmail.com>
2022-04-13 01:55:02 +0530

    12 Destructuring props and state: Destructuring props in function parameter

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
-import Counter from './components/Counter'
+import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Greet name="Diana" heroName="Wonder Woman" />
     </div>
   )
 }
